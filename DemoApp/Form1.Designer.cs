ba172b33c4fc11f6b26cc91c96cb4fe2f3f2d3a5
Paul Morris <paul.morri5@outlook.com>
2020-01-13 09:50:27 +0000

    Add project files.

---
+++ DemoApp/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace DemoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPaste = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox1
+            // 
+            this.picBox1.Location = new System.Drawing.Point(12, 12);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(249, 174);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox1.TabIndex = 0;
+            this.picBox1.TabStop = false;
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Location = new System.Drawing.Point(98, 196);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(75, 23);
+            this.btnPaste.TabIndex = 1;
+            this.btnPaste.Text = "Paste";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 234);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.picBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.Button btnPaste;
+    }
+}
+
---
+++ DemoApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using RDSDataObjects;
+
+namespace DemoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RDSDataObject dataObject = new RDSDataObject(Clipboard.GetDataObject());
+
+                if (Clipboard.ContainsImage())
+                {
+                    picBox1.Image = Clipboard.GetImage();
+                }
+                else if (dataObject.GetData("FileGroupDescriptorW") != null)
+                {
+                    var fileNames = dataObject.GetData("FileGroupDescriptorW") as string[];
+                    var fileStreams = dataObject.GetData("FileContents") as MemoryStream[];
+
+                    if (fileNames.Length > 0)
+                    {
+                        string fileName = fileNames[0];
+                        MemoryStream fileStream = fileStreams[0];
+
+                        picBox1.Image = Image.FromStream(fileStream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error:  {{{ex.Message}}}");
+            }
+        }
+    }
+}
